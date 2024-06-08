85e56c6eca5d765b9565af0ff690e8fde6bef1f9
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2024-06-09 07:33:25 +0900

    Update navigation-menu.tsx

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -1,35 +1,32 @@
-import {splitProps, type JSX, type ValidComponent} from "solid-js"
-
-import type {PolymorphicProps} from "@kobalte/core"
-import {Root} from "@kobalte/core/navigation-menu"
-
 import {cn} from "@/lib/utils"
-
+import type {PolymorphicProps} from "@kobalte/core"
 import {
-	Content,
-	Icon,
-	Item,
-	ItemDescription,
-	ItemLabel,
-	Portal,
+	// Content,
+	// Icon,
+	// Item,
+	// ItemDescription,
+	// ItemLabel,
+	// Portal,
+	Root,
 	Trigger,
 	Viewport,
-	type NavigationMenuContentProps as ContentProps,
-	type NavigationMenuItemDescriptionProps as ItemDescriptionProps,
-	type NavigationMenuItemLabelProps as ItemLabelProps,
-	type NavigationMenuItemProps as ItemProps,
+	// type NavigationMenuContentProps as ContentProps,
+	// type NavigationMenuItemDescriptionProps as ItemDescriptionProps,
+	// type NavigationMenuItemLabelProps as ItemLabelProps,
+	// type NavigationMenuItemProps as ItemProps,
 	type NavigationMenuRootProps,
 	type NavigationMenuTriggerProps as TriggerProps,
 	type NavigationMenuViewportProps as ViewportProps,
 } from "@kobalte/core/navigation-menu"
-export {Menu as NavigationMenuItem} from "@kobalte/core/navigation-menu"
+import {splitProps, type JSX, type ValidComponent} from "solid-js"
+// export {Menu as NavigationMenuItem} from "@kobalte/core/navigation-menu"
 
 type NavigationMenuProps<T extends ValidComponent = "ul"> = NavigationMenuRootProps<T> & {
 	class?: string | undefined
 	children?: JSX.Element
 }
 
-const NavigationMenu = <T extends ValidComponent = "ul">(props: PolymorphicProps<T, NavigationMenuProps<T>>) => {
+export const NavigationMenu = <T extends ValidComponent = "ul">(props: PolymorphicProps<T, NavigationMenuProps<T>>) => {
 	const [local, others] = splitProps(props as NavigationMenuProps, ["class", "children"])
 	return (
 		<Root
@@ -50,7 +47,7 @@ type NavigationMenuTriggerProps<T extends ValidComponent = "button"> = TriggerPr
 	class?: string | undefined
 }
 
-const NavigationMenuTrigger = <T extends ValidComponent = "button">(props: PolymorphicProps<T, NavigationMenuTriggerProps<T>>) => {
+export const NavigationMenuTrigger = <T extends ValidComponent = "button">(props: PolymorphicProps<T, NavigationMenuTriggerProps<T>>) => {
 	const [local, others] = splitProps(props as NavigationMenuTriggerProps, ["class"])
 	return (
 		<Trigger
@@ -62,31 +59,31 @@ const NavigationMenuTrigger = <T extends ValidComponent = "button">(props: Polym
 		/>
 	)
 }
-const NavigationMenuIcon = () => {
-	return (
-		<Icon aria-hidden="true">
-			{/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				class="relative top-px ml-1 size-3 transition duration-200 group-data-[expanded]/trigger:rotate-180 group-data-[orientation=vertical]/menu:-rotate-90 group-data-[orientation=vertical]/menu:group-data-[expanded]/trigger:rotate-90"
-			>
-				<path d="M6 9l6 6l6 -6" />
-			</svg>
-		</Icon>
-	)
-}
+// export const NavigationMenuIcon = () => {
+// 	return (
+// 		<Icon aria-hidden="true">
+// 			{/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+// 			<svg
+// 				xmlns="http://www.w3.org/2000/svg"
+// 				viewBox="0 0 24 24"
+// 				fill="none"
+// 				stroke="currentColor"
+// 				stroke-width="2"
+// 				stroke-linecap="round"
+// 				stroke-linejoin="round"
+// 				class="relative top-px ml-1 size-3 transition duration-200 group-data-[expanded]/trigger:rotate-180 group-data-[orientation=vertical]/menu:-rotate-90 group-data-[orientation=vertical]/menu:group-data-[expanded]/trigger:rotate-90"
+// 			>
+// 				<path d="M6 9l6 6l6 -6" />
+// 			</svg>
+// 		</Icon>
+// 	)
+// }
 
 type NavigationMenuViewportProps<T extends ValidComponent = "li"> = ViewportProps<T> & {
 	class?: string | undefined
 }
 
-const NavigationMenuViewport = <T extends ValidComponent = "li">(props: PolymorphicProps<T, NavigationMenuViewportProps<T>>) => {
+export const NavigationMenuViewport = <T extends ValidComponent = "li">(props: PolymorphicProps<T, NavigationMenuViewportProps<T>>) => {
 	const [local, others] = splitProps(props as NavigationMenuViewportProps, ["class"])
 	return (
 		<Viewport
@@ -99,78 +96,69 @@ const NavigationMenuViewport = <T extends ValidComponent = "li">(props: Polymorp
 	)
 }
 
-type NavigationMenuContentProps<T extends ValidComponent = "ul"> = ContentProps<T> & {
-	class?: string | undefined
-}
+// type NavigationMenuContentProps<T extends ValidComponent = "ul"> = ContentProps<T> & {
+// 	class?: string | undefined
+// }
 
-const NavigationMenuContent = <T extends ValidComponent = "ul">(props: PolymorphicProps<T, NavigationMenuContentProps<T>>) => {
-	const [local, others] = splitProps(props as NavigationMenuContentProps, ["class"])
-	return (
-		<Portal>
-			<Content
-				class={cn(
-					// base settings
-					"pointer-events-none absolute left-0 top-0 box-border p-4 focus:outline-none data-[expanded]:pointer-events-auto",
-					// base animation settings
-					"data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out",
-					// left to right
-					"data-[orientation=horizontal]:data-[motion=from-start]:slide-in-from-left-52 data-[orientation=horizontal]:data-[motion=to-end]:slide-out-to-right-52",
-					// right to left
-					"data-[orientation=horizontal]:data-[motion=from-end]:slide-in-from-right-52 data-[orientation=horizontal]:data-[motion=to-start]:slide-out-to-left-52",
-					// top to bottom
-					"data-[orientation=vertical]:data-[motion=from-start]:slide-in-from-top-52 data-[orientation=vertical]:data-[motion=to-end]:slide-out-to-bottom-52",
-					//bottom to top
-					"data-[orientation=vertical]:data-[motion=from-end]:slide-in-from-bottom-52 data-[orientation=vertical]:data-[motion=to-start]:slide-out-to-bottom-52",
-					local.class,
-				)}
-				{...others}
-			/>
-		</Portal>
-	)
-}
+// export const NavigationMenuContent = <T extends ValidComponent = "ul">(props: PolymorphicProps<T, NavigationMenuContentProps<T>>) => {
+// 	const [local, others] = splitProps(props as NavigationMenuContentProps, ["class"])
+// 	return (
+// 		<Portal>
+// 			<Content
+// 				class={cn(
+// 					// base settings
+// 					"pointer-events-none absolute left-0 top-0 box-border p-4 focus:outline-none data-[expanded]:pointer-events-auto",
+// 					// base animation settings
+// 					"data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out",
+// 					// left to right
+// 					"data-[orientation=horizontal]:data-[motion=from-start]:slide-in-from-left-52 data-[orientation=horizontal]:data-[motion=to-end]:slide-out-to-right-52",
+// 					// right to left
+// 					"data-[orientation=horizontal]:data-[motion=from-end]:slide-in-from-right-52 data-[orientation=horizontal]:data-[motion=to-start]:slide-out-to-left-52",
+// 					// top to bottom
+// 					"data-[orientation=vertical]:data-[motion=from-start]:slide-in-from-top-52 data-[orientation=vertical]:data-[motion=to-end]:slide-out-to-bottom-52",
+// 					//bottom to top
+// 					"data-[orientation=vertical]:data-[motion=from-end]:slide-in-from-bottom-52 data-[orientation=vertical]:data-[motion=to-start]:slide-out-to-bottom-52",
+// 					local.class,
+// 				)}
+// 				{...others}
+// 			/>
+// 		</Portal>
+// 	)
+// }
 
-type NavigationMenuLinkProps<T extends ValidComponent = "a"> = ItemProps<T> & {
-	class?: string | undefined
-}
+// type NavigationMenuLinkProps<T extends ValidComponent = "a"> = ItemProps<T> & {
+// 	class?: string | undefined
+// }
 
-const NavigationMenuLink = <T extends ValidComponent = "a">(props: PolymorphicProps<T, NavigationMenuLinkProps<T>>) => {
-	const [local, others] = splitProps(props as NavigationMenuLinkProps, ["class"])
-	return (
-		<Item
-			class={cn(
-				"block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors  hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-				local.class,
-			)}
-			{...others}
-		/>
-	)
-}
+// export const NavigationMenuLink = <T extends ValidComponent = "a">(props: PolymorphicProps<T, NavigationMenuLinkProps<T>>) => {
+// 	const [local, others] = splitProps(props as NavigationMenuLinkProps, ["class"])
+// 	return (
+// 		<Item
+// 			class={cn(
+// 				"block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors  hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+// 				local.class,
+// 			)}
+// 			{...others}
+// 		/>
+// 	)
+// }
 
-type NavigationMenuLabelProps<T extends ValidComponent = "div"> = ItemLabelProps<T> & {
-	class?: string | undefined
-}
+// type NavigationMenuLabelProps<T extends ValidComponent = "div"> = ItemLabelProps<T> & {
+// 	class?: string | undefined
+// }
 
-const NavigationMenuLabel = <T extends ValidComponent = "div">(props: PolymorphicProps<T, NavigationMenuLabelProps<T>>) => {
-	const [local, others] = splitProps(props as NavigationMenuLabelProps, ["class"])
-	return <ItemLabel class={cn("text-sm font-medium leading-none", local.class)} {...others} />
-}
+// export const NavigationMenuLabel = <T extends ValidComponent = "div">(props: PolymorphicProps<T, NavigationMenuLabelProps<T>>) => {
+// 	const [local, others] = splitProps(props as NavigationMenuLabelProps, ["class"])
+// 	return <ItemLabel class={cn("text-sm font-medium leading-none", local.class)} {...others} />
+// }
 
-type NavigationMenuDescriptionProps<T extends ValidComponent = "div"> = ItemDescriptionProps<T> & {
-	class?: string | undefined
-}
+// type NavigationMenuDescriptionProps<T extends ValidComponent = "div"> = ItemDescriptionProps<T> & {
+// 	class?: string | undefined
+// }
 
-const NavigationMenuDescription = <T extends ValidComponent = "div">(props: PolymorphicProps<T, NavigationMenuDescriptionProps<T>>) => {
-	const [local, others] = splitProps(props as NavigationMenuDescriptionProps, ["class"])
-	return <ItemDescription class={cn("text-sm leading-snug text-muted-foreground", local.class)} {...others} />
-}
-
-export {
-	NavigationMenu,
-	NavigationMenuContent,
-	NavigationMenuDescription,
-	NavigationMenuIcon,
-	NavigationMenuLabel,
-	NavigationMenuLink,
-	NavigationMenuTrigger,
-	NavigationMenuViewport,
-}
+// export const NavigationMenuDescription = <T extends ValidComponent = "div">(
+// 	props: PolymorphicProps<T, NavigationMenuDescriptionProps<T>>,
+// ) => {
+// 	const [local, others] = splitProps(props as NavigationMenuDescriptionProps, ["class"])
+// 	return <ItemDescription class={cn("text-sm leading-snug text-muted-foreground", local.class)} {...others} />
+// }
